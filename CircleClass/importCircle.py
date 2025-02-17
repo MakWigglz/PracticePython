bf7180c8a7eec54941ec65d88fb61fcf90c8f076
MakWigglz <wigglz734@gmail.com>
2025-02-17 14:42:58 +0200

    Create importCircle.py

---
+++ CircleClass/importCircle.py
@@ -0,0 +1,9 @@
+from ClassCircleArea import Circle
+
+circle_1 = Circle(42)
+circle_2 = Circle(17)
+
+print(circle_1)
+print(circle_2)
+print(f"Area of circle_1: {circle_1.calculate_area()}")
+print(f"Area of circle_2: {circle_2.calculate_area()}")
