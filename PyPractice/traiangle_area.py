1f574a618dcdf8e2954560141c0677fbd3dfe5f4
MakWigglz <wigglz734@gmail.com>
2025-05-07 21:47:06 +0300

    Create traiangle_area.py

---
+++ PyPractice/traiangle_area.py
@@ -0,0 +1,20 @@
+def calculate_triangle_area(base: float, height: float):
+
+    area = 0.5 * base * height
+    return area
+
+base_length = 5.5
+triangle_height = 8.2
+area_of_triangle = calculate_triangle_area(base_length, triangle_height)
+print(f"the area of Mak's triangle is: {area_of_triangle}")
+
+
+
+
+
+
+
+``` Output: (new-matplot-venv) amakki@As-MacBook-Pro python_functions % python maks_triangle_area.py
+the area of Mak's triangle is: 22.549999999999997
+(new-matplot-venv) amakki@As-MacBook-Pro python_functions % 
+```
